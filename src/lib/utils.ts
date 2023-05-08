027e4ac52946d7270388580e1a8b72c5d943c7d3
Eric Skram <eric@skram.com>
2023-05-08 08:54:25 -0600

    Add some documentation to getNextIndex

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,29 +1,42 @@
 interface NextIndexInput {
+  /** Currently selected item index. */
   currentIndex: number;
+  /** The number of items in the list. */
   itemCount: number;
+  /**
+   * The number of items to traverse in the list. Eg:
+   * +10 would move down 10 items.
+   * -10 would move up 10 items.
+   */
   moveAmount: number;
 }
 
+/**
+ * Returns the next index in an array based on the current index
+ * and move amount. If the next index is out of bounds, it will
+ * wrap around to the other end of the array.
+ */
 export function getNextIndex({
   currentIndex,
   itemCount,
   moveAmount,
-}: NextIndexInput) {
+}: NextIndexInput): number {
   // Return early if there are no items.
   if (itemCount === 0) {
     return 0;
   }
-  // Compute the index of the last item in the list.
+  // Get the index of the last item in the list.
   const lastItemIndex = itemCount - 1;
   // Compute the next index by adding the move amount to the current index.
   const nextIndex = currentIndex + moveAmount;
-  // Wrap from the top of the list back to the bottom.
+  // If the computed index is negative, wrap back to the bottom of the list.
   if (nextIndex < 0) {
     return lastItemIndex;
   }
-  // Wrap from the bottom of the list back to the top.
+  // If the computed index is out of bounds, wrap back to the top of the list.
   if (nextIndex > lastItemIndex) {
     return 0;
   }
+  // Otherwise, return the computed index.
   return nextIndex;
 }
