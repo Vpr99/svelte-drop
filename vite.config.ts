4ba947d30a8d95ed40e2d03fa913a8b09f010b5d
Eric Skram <eric@skram.com>
2023-05-12 11:30:51 -0600

    Pin to specific dependency versions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import icons from "unplugin-icons/vite";
 import kitDocs from "@svelteness/kit-docs/node";
 
 export default defineConfig({
-  plugins: [icons({ compiler: "svelte" }), kitDocs(), sveltekit()],
+  plugins: [
+    // @ts-expect-error type mismatch with unplugin-icons' vite export.
+    icons({ compiler: "svelte" }),
+    kitDocs(),
+    sveltekit(),
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
