d3635068baf8d5f5a490f0d8e80dbbeddc34014a
Eric Skram <eric@skram.com>
2023-05-12 11:26:07 -0600

    Remove config.package from svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import adapter from "@sveltejs/adapter-vercel";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
-
   kit: { adapter: adapter() },
-  package: {
-    dir: "client",
-    emitTypes: true,
-  },
 };
 
 export default config;
