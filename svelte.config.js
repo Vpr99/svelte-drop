f651b4804b9f3255a318c85a59456917624925f0
Eric Skram <eric@skram.com>
2023-05-12 11:24:33 -0600

    Bootstrap kit-docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import adapter from "@sveltejs/adapter-vercel";
-import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  extensions: [".svelte", ".md"],
+
   kit: { adapter: adapter() },
+  package: {
+    dir: "client",
+    emitTypes: true,
+  },
 };
 
 export default config;
